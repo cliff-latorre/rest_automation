a761bc27280474229d119a375cf9afbe1c54dcd3
cliff-latorre <teocafe@gmail.com>
2022-01-07 14:51:19 -0500

    patch put added

--- src/test/java/org/rest/ReqResTest.java
+++ src/test/java/org/rest/ReqResTest.java
@@ -1,62 +1,117 @@
 package org.rest;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.equalTo;
+
+
 public class ReqResTest {
 
+    @Before
+    public void setup() {
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+
     @Test
-    public void loginTest(){
-         RestAssured
-                .given()
-                .log()
-                .all()
+    public void loginTest() {
+        given()
                 .contentType(ContentType.JSON)
                 .body("{\n" +
                         "    \"email\": \"eve.holt@reqres.in\",\n" +
                         "    \"password\": \"cityslicka\"\n" +
                         "}")
-                .post("https://reqres.in/api/login")
+                .post("login")
                 .then()
-                .log()
-                .all()
-                .statusCode(200)
-                 .body("token", notNullValue());
+                .statusCode(HttpStatus.SC_OK)
+                .body("token", notNullValue());
 
 
     }
+
+    @Ignore
     @Test
-    public void registerTest(){
-        String response = RestAssured
-                .given()
+    public void registerTest() {
+        String response = given()
                 .contentType(ContentType.JSON)
                 .body("{\n" +
                         "    \"email\": \"eve.holt@reqres.in\",\n" +
                         "    \"password\": \"pistol\"\n" +
                         "}")
-                .post("https://reqres.in/api/register")
+                .post("register")
                 .then()
                 .extract()
                 .asString();
 
         System.out.println(response);
     }
+
     @Test
-    public void getSingleUserTest(){
-        RestAssured
-                .given()
-                .log()
-                .all()
+    public void getSingleUserTest() {
+        given()
                 .contentType(ContentType.JSON)
-                .get("https://reqres.in/api/users/2")
+                .get("users/2")
                 .then()
-                .log()
-                .all()
-                .statusCode(200)
+                .statusCode(HttpStatus.SC_OK)
                 .body("data.id", equalTo(2));
+    }
 
+    @Test
+    public void deleteUserTest() {
+        given()
+                .delete("users/2")
+                .then()
+                .statusCode(HttpStatus.SC_NO_CONTENT);
 
+    }
+
+    @Test
+    public void patchUserTest() {
+        String nameUpdate = given()
+                .when()
+                .body("{\n" +
+                        "    \"name\": \"morpheus\",\n" +
+                        "    \"job\": \"zion resident\",\n" +
+                        "    \"updatedAt\": \"2022-01-07T18:30:32.797Z\"\n" +
+                        "}")
+                .patch("users/2")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .jsonPath().getString("name");
+        assertThat(nameUpdate, equalTo("morpheus"));
+    }
+
+    @Test
+    public void putUserTest() {
+        String nameUpdate = given()
+                .when()
+                .body("{\n" +
+                        "    \"name\": \"morpheus1\",\n" +
+                        "    \"job\": \"zion resident2\",\n" +
+                        "    \"updatedAt\": \"2022-01-07T18:30:32.797Z\"\n" +
+                        "}")
+                .put("users/2")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .jsonPath().getString("job");
+        assertThat(nameUpdate, equalTo("zion resident"));
     }
 }
